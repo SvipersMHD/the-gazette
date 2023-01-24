c01f3698afe5f8fe7830a9b1b437f77d3cfb1e21
SvipersMHD <zohaibadnan29.10@gmail.com>
2023-01-24 12:36:35 +0100

    fin grid gauche

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var leClick = document.querySelector(".liste__titre1");
-var content = document.querySelector("contenu1");
-
-leClick.addEventListener("click", function() {
-    content.classList.toggle("--isActive");
-});
-
-
