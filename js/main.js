d94daa0830f811316eb5426b6e77683a493362a1
SvipersMHD <zohaibadnan29.10@gmail.com>
2023-01-25 10:41:43 +0100

    finito pipo

--- js/main.js
+++ js/main.js
@@ -2,4 +2,28 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
     spaceBetween: 0,
     loop: true,
+    autoplay: {
+      delay: 2000,
+    },
   });
+
+  var arrow = document.querySelector(".arrow"); 
+  var scrolling = 0;
+  var body = document.querySelector("body")
+
+  window.addEventListener("scroll", function() {
+    scrolling =  window.scrollY;
+
+    if(scrolling >= 300){
+      arrow.classList.add("--isActive")
+    }
+    else {
+      arrow.classList.remove("--isActive")
+    }
+  })
+
+
+
+  arrow.addEventListener("click", function() {
+    document.documentElement.scrollTop = 0;
+  })
